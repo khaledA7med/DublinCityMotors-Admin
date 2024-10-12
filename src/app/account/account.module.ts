732c9659252593e2b9cd3511f06fac33fa1cd28f
Khalid Ahmed mohamed <khalid.ahmed@oasisoft.net>
2024-10-12 18:25:09 +0300

    []

--- src/app/account/account.module.ts
+++ src/app/account/account.module.ts
@@ -2,9 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AccountRoutingModule } from './account-routing.module';
+import { JwtInterceptor } from '../core/helpers/jwt.interceptor';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule, AccountRoutingModule],
+  providers: [
+    {
+      useClass: JwtInterceptor,
+      multi: true,
+      provide: HTTP_INTERCEPTORS,
+    },
+  ],
 })
 export class AccountModule {}
--- src/app/core/helpers/jwt.interceptor.ts
+++ src/app/core/helpers/jwt.interceptor.ts
@@ -3,16 +3,30 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
+  constructor(private auth: AuthService) {}
 
-  constructor() {}
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    // add authorization header with jwt token if available
+    let currentUser = this.auth.currentUserToken;
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    if (currentUser) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${currentUser}`,
+        },
+      });
+    }
+
     return next.handle(request);
   }
 }
