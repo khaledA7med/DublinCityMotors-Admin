9844bf10ab8ee893f5b9ad1b2ba68048aa1164f5
Khalid Ahmed mohamed <khalid.ahmed@oasisoft.net>
2024-11-01 16:56:15 +0200

    []

--- src/app/extra-pages/not-found/not-found.component.ts
+++ src/app/extra-pages/not-found/not-found.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: "app-not-found",
-  templateUrl: "./not-found.component.html",
-  styleUrls: ["./not-found.component.scss"],
+  selector: 'app-not-found',
+  templateUrl: './not-found.component.html',
+  styleUrls: ['./not-found.component.scss'],
 })
-export class NotFoundComponent implements OnInit {
-  year: number = new Date().getFullYear();
+export class NotFoundComponent {
+  constructor(private router: Router) {}
 
-  constructor() {}
-
-  ngOnInit(): void {}
+  goHome(): void {
+    this.router.navigate(['/']); // Navigate to the home page
+  }
 }
